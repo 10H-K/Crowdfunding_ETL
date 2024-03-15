6abb834d5e61d6fa1d61704c2949ceac4c3a369c
10H-K <152930492+10H-K@users.noreply.github.com>
2024-03-14 20:42:31 -0400

    files

--- Crowdfunding_ETL_Schema.sql
+++ Crowdfunding_ETL_Schema.sql
@@ -53,3 +53,8 @@ REFERENCES "category" ("category_id");
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
 
+select * from contacts
+select * from campaign
+select * from subcategory
+select * from category
+
